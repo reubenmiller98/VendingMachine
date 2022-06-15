81c9bd1c91273109655a7c68b3964934fc8d9ba3
Wendy Paddock <wjpaddock@gmail.com>
2022-06-15 16:25:52 -0400

    commit.capstonesmap.containskey(Wooben Vending Machine)

--- src/main/java/com/techelevator/Item.java
+++ src/main/java/com/techelevator/Item.java
@@ -6,35 +6,8 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class Item {
-    private String name;
-    private String id;
-    private double price;
-    private int itemQty;
-    private String type;
+
     public int numberSold;
-
-    VendingMachine vendingMachine = new VendingMachine();
-
-    public Item() {
-    }
-
-    File vendingMachineList = new File("vendingmachine.csv");
-
-    public String getName() {
-        return name;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public String getType() {
-        return type;
-    }
 
     public int getNumberSold() {
         return numberSold;
